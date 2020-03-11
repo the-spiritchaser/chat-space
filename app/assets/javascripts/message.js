8b1fc668b52ae981161e94b2bb9b833003b491fe
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-03-12 02:57:10 +0900

    Add html template to messages.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,46 @@
 $(function(){
+
+  function buildHTML(message){
+    if (message.image) {
+      var html = 
+                  `<div class="message">
+                    <div class="message__label">
+                      <div class="message__label__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="message__label__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message__content">
+                      <p class="message__content__text">
+                        ${message.text}
+                      </p>
+                    </div>
+                    <img class="message__content__image" src=${message.image} alt="画像です"></img>
+                  </div>`
+      return html;
+    } else {
+      var html = 
+                  `<div class="message">
+                    <div class="message__label">
+                      <div class="message__label__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="message__label__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message__content">
+                      <p class="message__content__text">
+                        ${message.text}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    };
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
 
@@ -11,6 +53,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('form')[0].reset()
     })
   })
 });
