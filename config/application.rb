24f611db47407250225c666303e818d55ba8f43f
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-03-12 12:00:08 +0900

    Add TimeZone to application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Osaka'
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
